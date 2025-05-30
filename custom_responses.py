ef2297888334ed0a391601c6de83bcf3bc3f75d8
mr8luesky <savahulup@gmai.com>
2025-05-30 17:30:58 +0300

    Don't touch priorities, just set deadline for pieces

--- custom_responses.py
+++ custom_responses.py
@@ -14,15 +14,19 @@ from starlette.types import Send
 logger = logging.getLogger(__name__)
 
 
-
 class PieceManager:
     preload_pieces = 30
     max_prior_from = 15
     """torrent_info: lt.torrent_info = None, torrent_handle: lt.torrent_handle,
                  file_index: int = -1"""
 
-    def __init__(self, session: lt.session, th: lt.torrent_handle, ti: lt.torrent_info,
-                 file_index: int):
+    def __init__(
+        self,
+        session: lt.session,
+        th: lt.torrent_handle,
+        ti: lt.torrent_info,
+        file_index: int,
+    ):
         self.piece_buffer = {}
         self.piece_wait = {}
         self.ses = session
@@ -40,25 +44,21 @@ class PieceManager:
     def file_size(self):
         return self.ti.files().file_size(self.fi)
 
-    def initiate(self, b_start: int):
+    def initiate(self, b_start: int, b_end: int):
         p_start, _ = self.bytes_to_piece_offset(b_start)
+        p_end, _ = self.bytes_to_piece_offset(b_end)
         self.th.prioritize_pieces(
-            (i, 7) for i in range(p_start, p_start + self.preload_pieces) if not self.th.have_piece(i))
+            (i, 4) for i in range(p_start, p_end) if not self.th.have_piece(i)
+        )
+        for p in range(p_start, p_start + self.preload_pieces):
+            self.th.set_piece_deadline(p, p - p_start, 0)
 
-    def cleanup(self):
-        ...
-
-    def prioritize_pieces(self, prior: int, start: int, end: int):
-        self.th.prioritize_pieces(
-            (i, prior) for i in range(start, end)
-        ) 
-        logger.debug(
-            f"Prioritizing from {start} to {min(end + self.preload_pieces, end)} with {prior}")
+    def cleanup(self): ...
 
     async def iter_pieces(self, b_start: int, b_end: int = -1) -> AsyncGenerator[bytes]:
-        self.initiate(b_start)
         if b_end == -1:
             b_end = self.file_size()
+        self.initiate(b_start, b_end)
         start, start_offset = self.bytes_to_piece_offset(b_start)
         end, end_offset = self.bytes_to_piece_offset(b_end)
         for piece_id in range(start, end + 1):
@@ -71,6 +71,15 @@ class PieceManager:
             self.last_piece = piece_id
             yield piece[fr:to]
 
+    def increment_queue(self, piece_id: int):
+        self.piece_wait[piece_id] = self.piece_wait.get(piece_id, 0) + 1
+
+    def decrement_queue(self, piece_id: int):
+        self.piece_wait[piece_id] -= 1
+        if not self.piece_wait[piece_id]:
+            self.piece_wait.pop(piece_id)
+            self.piece_buffer.pop(piece_id)
+
     async def get_piece(self, piece_id: int, timeout_s: int = 15):
         if not self.th.have_piece(piece_id + self.preload_pieces):
             self.th.piece_priority(piece_id + self.preload_pieces, 4)
@@ -78,18 +87,23 @@ class PieceManager:
             self.th.piece_priority(piece_id + self.max_prior_from, 6)
         if not self.th.have_piece(piece_id):
             self.th.piece_priority(piece_id, 7)
-        self.th.set_piece_deadline(piece_id, 0, 1)
-
+        self.th.set_piece_deadline(
+            piece_id + self.preload_pieces, piece_id - self.start, 0
+        )
+        self.increment_queue(piece_id)
         finish = time() + timeout_s
 
-        self.piece_wait[piece_id] = self.piece_wait.get(piece_id, 0) + 1
         while not self.th.have_piece(piece_id) and finish > time():
             await sleep(0.005)
 
         if not self.th.have_piece(piece_id):
-            raise AttributeError(f"Don't have {piece_id} after {timeout_s}! "
-                                 f"Priority: {self.th.piece_priority(piece_id)}")
+            self.decrement_queue(piece_id)
+            raise AttributeError(
+                f"Don't have {piece_id} after {timeout_s}! "
+                f"Priority: {self.th.piece_priority(piece_id)}"
+            )
 
+        self.th.read_piece(piece_id)
 
         while piece_id not in self.piece_buffer and finish > time():
             alerts = self.ses.pop_alerts()
@@ -100,12 +114,9 @@ class PieceManager:
             await sleep(0.005)
 
         if piece_id not in self.piece_buffer:
+            self.decrement_queue(piece_id)
             raise AttributeError(f"{piece_id} not read in {timeout_s}")
         buffer = self.piece_buffer[piece_id]
-        self.piece_wait[piece_id] -= 1
-        if not self.piece_wait[piece_id]:
-            self.piece_wait.pop(piece_id)
-            self.piece_buffer.pop(piece_id)
 
         return buffer
 
@@ -131,38 +142,46 @@ class LoadingTorrentFileResponse(FileResponse):
                 self.pm.cleanup()
                 return
             yield buffer, True
-        yield b'', False
+        yield b"", False
 
     async def _handle_simple(self, send: Send, send_header_only: bool) -> None:
-        await send({"type": "http.response.start", "status": self.status_code, "headers": self.raw_headers})
+        await send(
+            {
+                "type": "http.response.start",
+                "status": self.status_code,
+                "headers": self.raw_headers,
+            }
+        )
         if send_header_only:
             await send({"type": "http.response.body", "body": b"", "more_body": False})
         else:
             async for body, more_body in self._download_range(0):
-                await send({"type": "http.response.body",
-                                   "body": body,
-                                   "more_body": more_body})
+                await send(
+                    {"type": "http.response.body", "body": body, "more_body": more_body}
+                )
 
     async def _handle_single_range(
-            self, send: Send, start: int, end: int, file_size: int, send_header_only: bool
+        self, send: Send, start: int, end: int, file_size: int, send_header_only: bool
     ) -> None:
         self.headers["content-range"] = f"bytes {start}-{end - 1}/{file_size}"
         self.headers["content-length"] = str(end - start)
-        await send({"type": "http.response.start", "status": 206, "headers": self.raw_headers})
+        await send(
+            {"type": "http.response.start", "status": 206, "headers": self.raw_headers}
+        )
         if send_header_only:
             await send({"type": "http.response.body", "body": b"", "more_body": False})
         else:
             async for body, more_body in self._download_range(start, end):
-                await send({"type": "http.response.body",
-                            "body": body,
-                            "more_body": more_body})
+                await send(
+                    {"type": "http.response.body", "body": body, "more_body": more_body}
+                )
 
     async def _handle_multiple_ranges(
-            self,
-            send: Send,
-            ranges: list[tuple[int, int]],
-            file_size: int,
-            send_header_only: bool,
+        self,
+        send: Send,
+        ranges: list[tuple[int, int]],
+        file_size: int,
+        send_header_only: bool,
     ) -> None:
         # In firefox and chrome, they use boundary with 95-96 bits entropy (that's roughly 13 bytes).
         boundary = token_hex(13)
@@ -171,15 +190,27 @@ class LoadingTorrentFileResponse(FileResponse):
         )
         self.headers["content-range"] = f"multipart/byteranges; boundary={boundary}"
         self.headers["content-length"] = str(content_length)
-        await send({"type": "http.response.start", "status": 206, "headers": self.raw_headers})
+        await send(
+            {"type": "http.response.start", "status": 206, "headers": self.raw_headers}
+        )
         if send_header_only:
             await send({"type": "http.response.body", "body": b"", "more_body": False})
         else:
             for start, end in ranges:
-                await send({"type": "http.response.body", "body": header_generator(start, end), "more_body": True})
+                await send(
+                    {
+                        "type": "http.response.body",
+                        "body": header_generator(start, end),
+                        "more_body": True,
+                    }
+                )
                 async for body, _ in self._download_range(start, end):
-                    await send({"type": "http.response.body", "body": body, "more_body": True})
-                await send({"type": "http.response.body", "body": b"\n", "more_body": True})
+                    await send(
+                        {"type": "http.response.body", "body": body, "more_body": True}
+                    )
+                await send(
+                    {"type": "http.response.body", "body": b"\n", "more_body": True}
+                )
             await send(
                 {
                     "type": "http.response.body",
