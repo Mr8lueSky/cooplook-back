98e15dc4285f022bc17889fe119087b85e750cc3
Saveli Khulup <savahulup@gmail.com>
2025-06-12 08:43:52 +0300

    For DEV: read templates every time; For other: read once

--- templates/__init__.py
+++ templates/__init__.py
@@ -1,20 +1,29 @@
 from typing import Any
 
 from fastapi.responses import HTMLResponse
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, Template
 
+from config import ENV
 from exceptions import HTTPException
 
 env = Environment(
     loader=FileSystemLoader(searchpath="templates"),
 )
 
+templates = {}
+
+def get_template(name: str) -> Template:
+    return env.get_template(f"{name}.html")
 
 def get_template_response(
     name: str, data: dict[str, Any] | None = None, exceptions: list[HTTPException] | None = None
 ):
-    data = data or {}
-    exceptions = exceptions or []
-    return HTMLResponse(
-        env.get_template(f"{name}.html").render(**data, exceptions=exceptions)
-    )
+    if ENV == "DEV":
+        data = data or {}
+        exceptions = exceptions or []
+        return HTMLResponse(
+            get_template(name).render(**data, exceptions=exceptions)
+        )
+    if name not in templates:
+        templates[name] = get_template(name)
+    return templates[name]
