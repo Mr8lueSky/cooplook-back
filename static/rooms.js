91949b92bbd6f92ffbb79a58435f141dde3f3280
Saveli Khulup <savahulup@gmail.com>
2025-08-27 11:25:45 +0300

    Remove static files

--- static/rooms.js
+++
@@ -1,6 +0,0 @@
-let FROM_LINK_URL = `/rooms/from_link`;
-let FROM_TORRENT_URL = `/rooms/from_torrent`;
-let sourcesToLinks = {
-        [FROM_LINK]: FROM_LINK_URL,
-        [FROM_TORRENT]: FROM_TORRENT_URL
-}
